94491d363f1214cae2a99fe97755d3ccf50fbc05
octocrux <cate.petrusevich@gmail.com>
2016-04-09 16:51:17 +0300

    Add input controls

--- client/app/routes/new-order.js
+++ client/app/routes/new-order.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
+import Order from 'client/models/order';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      restaurants: [ { name: "pzz", id: 1 }, { name: "tempo", id: 2 } ],
+      order: {
+        name: "Вася",
+        phone: "+375 33 333 33 33",
+        comment: "Маргарита 1шт без сыра",
+        amount: "100 000"
+      }
+    }
+  }
 });
