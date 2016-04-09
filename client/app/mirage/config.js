c5f148c6ec29c86671471bcde50cea809479b5c6
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-04-09 14:51:31 +0300

    Add test endpoint for /orders

--- client/app/mirage/config.js
+++ client/app/mirage/config.js
@@ -1,2 +1,20 @@
 export default function() {
+
+  this.get('/orders', function(db, request) {
+    return {
+      data: [
+        {
+          id: 1,
+          type: "order",
+          attributes: {
+            organizerName: "Пицца Темпо",
+            orderTime: "14:00",
+            moneyRequired: 300000,
+            moneyCurrent: 100000
+          }
+        },
+      ]
+    };
+  });
+
 }
