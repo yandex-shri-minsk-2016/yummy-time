5b4fed87a477a6e6aae5c40f3265fea7d7f51112
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-04-11 13:10:41 +0300

    Rename `tabs-list` component into `order-list-tabs`

--- client/tests/integration/components/order-list-tabs-test.js
+++ client/tests/integration/components/order-list-tabs-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('tabs-list', 'Integration | Component | tabs list', {
+moduleForComponent('order-list-tabs', 'Integration | Component | order list tabs', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{tabs-list}}`);
+  this.render(hbs`{{order-list-tabs}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#tabs-list}}
+    {{#order-list-tabs}}
       template block text
-    {{/tabs-list}}
+    {{/order-list-tabs}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
