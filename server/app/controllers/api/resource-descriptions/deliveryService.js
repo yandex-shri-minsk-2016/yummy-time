8540393849c26412747582622646286f311ca24a
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-04-15 00:24:10 +0300

    Удалён неиспользуемый ресурс `deliverService` и его описание

--- server/app/controllers/api/resource-descriptions/deliveryService.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  urlTemplates: {
-    'self': 'http://127.0.0.1:3000/deliveryService/{id}',
-    'relationship': 'http://127.0.0.1:3000/deliveryService/{ownerId}/relationships/{path}'
-  }
-};
