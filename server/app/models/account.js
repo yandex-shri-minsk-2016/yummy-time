522285e00cb32a5b6e747548f405ba658f462153
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-04-14 11:29:56 +0300

    Scaffold account model

---
+++ server/app/models/account.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const bcrypt = require('bcrypt-nodejs');
+const mongoose = require('mongoose');
+
+const accountSchema = mongoose.Schema({
+  email: { type: String, required: true },
+  hashed_password: { type: String, required: true },
+  name: { type: String, default: '' },
+  phone: { type: String, default: '' },
+});
+
+accountSchema
+  .virtual('password')
+  .set(function(password) {
+    this._password = password;
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+accountSchema.path('email').validate(function(email, callback) {
+  const Account = mongoose.model('Account');
+
+  if (this.isNew || this.isModified('email')) {
+    Account.find({email: email}).exec(function(err, accounts) {
+      callback(!err && accounts.length === 0)
+    });
+  } else {
+    callback(true);
+  }
+}, 'Email already exists');
+
+accountSchema.methods = {
+
+  /**
+   * Create an unique salt sequence
+   *
+   * @return {Number}
+   * @api public
+   */
+  makeSalt() {
+    return bcrypt.genSaltSync(8);
+  },
+
+  /**
+   * Encrypt password
+   *
+   * @param {String} password
+   * @return {String}
+   * @api public
+   */
+  encryptPassword(password) {
+    return bcrypt.hashSync(password, this.makeSalt());
+  },
+
+  /**
+   * Ensure the password is valid
+   *
+   * @param {String} password
+   * @return {Boolean}
+   * @api public
+   */
+  authenticate(password) {
+    return bcrypt.compareSync(password, this.hashed_password);
+  }
+};
+
+accountSchema.statics = {
+
+  /**
+   * Load instance
+   *
+   * @param {Object} options
+   * @param {Function} callback
+   * @api private
+   */
+  load(options, callback) {
+    options.select = options.select || 'name';
+    return this.findOne(options.criteria)
+      .select(options.select)
+      .exec(callback);
+  }
+};
+
+module.exports = mongoose.model('Account', accountSchema);
