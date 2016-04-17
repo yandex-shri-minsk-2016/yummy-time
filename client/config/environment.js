dc18272bcb65cf4c1e1b7d95afae60772cd236ff
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-04-17 10:36:36 +0300

    Use token authorizer to attach JWT to outgoing requests

--- client/config/environment.js
+++ client/config/environment.js
@@ -19,6 +19,10 @@ module.exports = function(environment) {
     }
   };
 
+  ENV['ember-simple-auth'] = {
+    authorizer: 'authorizer:token',
+  }
+
   ENV['ember-simple-auth-token'] = {
     serverTokenEndpoint: '/auth/token',
     authorizationPrefix: 'JWT ',
