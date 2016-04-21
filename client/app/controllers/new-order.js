787201814620a8a5d0fdb6e65c599f09bc01eb3c
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-04-21 23:12:17 +0300

    Bind new order to current account
    Closes #79.

--- client/app/controllers/new-order.js
+++ client/app/controllers/new-order.js
@@ -2,9 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    createOrder(vendor, attrs) {
+    createOrder(vendor, account, attrs) {
       const order = this.store.createRecord('order', attrs);
       order.set('vendor', vendor);
+      order.set('manager', account);
       order.save();
       this.transitionToRoute('orders');
     }
