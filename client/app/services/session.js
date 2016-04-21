26d9f165fe1c0abe3c3a3edfc87b4bcf5026cd21
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-04-21 13:54:29 +0300

    Load current account when session is authenticated
    Inject session service into application controller to ensure the template has
    access to it.

---
+++ client/app/services/session.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import ESASession from 'ember-simple-auth/services/session';
+
+export default ESASession.extend({
+  store: Ember.inject.service(),
+
+  loadCurrentAccount: Ember.observer('isAuthenticated', function() {
+    if (this.get('isAuthenticated')) {
+      const jwt = Ember.getOwner(this).lookup('authenticator:jwt');
+      const payload = jwt.getTokenData(this.get('session.authenticated.token'));
+
+      this.get('store').find('account', payload).then((account) => {
+        this.set('account', account)
+      });
+    } else {
+      this.set('account', null);
+    }
+  })
+});
