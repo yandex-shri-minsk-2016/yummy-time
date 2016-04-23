91a07955036b53091db1cdf66dded64a84222a64
Evgeny Alisyonok <ealisyonok@gmail.com>
2016-04-23 19:15:07 +0300

    fix styles

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import BodyClassMixin from 'ember-body-class/mixins/body-class';
 
-export default Ember.Route.extend(ApplicationRouteMixin, {
+export default Ember.Route.extend(ApplicationRouteMixin, BodyClassMixin, {
   session: Ember.inject.service(),
 
   beforeModel() {
