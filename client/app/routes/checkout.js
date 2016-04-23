7a9d8fccf05fd932fa5c8be503e3f885ecb3e29a
rei-li <rei-li@mail.ru>
2016-04-23 17:59:18 +0300

    fix portions test data

--- client/app/routes/checkout.js
+++ client/app/routes/checkout.js
@@ -4,17 +4,17 @@ var portions =  [{
  id: 1,
  text: 'Grand Old Mansion',
  cost: '21 0000',
- paid: 'San Francisco'
+ paid: 'false'
 }, {
   id: 2,
   text: 'Grand Old Mansion',
   cost: '20 0000',
-  paid: 'San Francisco'
+  paid: 'false'
 }, {
   id: 3,
   text: 'Grand Old Mansion',
   cost: '50 0000',
-  paid: 'San Francisco'
+  paid: 'false'
 }];
 
 
