aea778ed1d8657b9f57db1b181a5b48717072510
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-04-26 13:44:03 +0300

    Use computed property to define account’s display name

--- client/app/models/account.js
+++ client/app/models/account.js
@@ -1,8 +1,19 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   email: DS.attr('string'),
   password: DS.attr('string'),
-  phone: DS.attr('string')
+  phone: DS.attr('string'),
+
+  /**
+   * Public name of the account
+   *
+   * @return {String} The specified name of the account or email if no name
+   *                  is available.
+   */
+  displayName: Ember.computed('name', 'email', function() {
+    return this.get('name') || this.get('email');
+  })
 });
