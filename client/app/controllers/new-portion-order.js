00785fbc71d6715c0903005b37cba40a1cfbc3d0
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-04-27 00:34:38 +0300

    Allow create a new portion to order via form
    Closes #62.

---
+++ client/app/controllers/new-portion-order.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    addToOrder(order, account, attrs) {
+      const portion = this.store.createRecord('portion', attrs);
+      portion.set('order', order);
+      portion.set('owner', account);
+      portion.save().then(() => {
+        order.get('portions').pushObject(portion);
+        order.save();
+      });
+    }
+  }
+});
