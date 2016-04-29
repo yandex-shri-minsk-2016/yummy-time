7240dd7db3c9ad3f6d4a42a89a54a936b458868a
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-04-29 20:27:54 +0300

    Allow change order active state vi checkout form
    Closes #18.

--- client/app/components/f-checkout.js
+++ client/app/components/f-checkout.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  order: Ember.computed.alias('model.order'),
+
+  actions: {
+    toggleActiveState() {
+      const order = this.get('order');
+      order.toggleProperty('active');
+      order.save();
+    }
+  }
 });
