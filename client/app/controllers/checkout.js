8708d8ebb59985bf724c90bdea102ba90a819dc0
rei-li <rei-li@mail.ru>
2016-04-30 17:42:55 +0300

    send message to server

--- client/app/controllers/checkout.js
+++ client/app/controllers/checkout.js
@@ -1,16 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service(),
+  socketService: Ember.inject.service('socket-io'),
+
   actions: {
     send(data) {
 
       console.log(data.message);
-      $.ajax({
-        type: "POST",
-        url: "http://localhost:3000",
-        data: { msg: data.message }
-      })
-      // this.transitionToRoute('notify', data.message);
+
+      this._super.apply(this, arguments);
+
+      var socketIO = this.get('socketService').socketFor('http://localhost:3000/');
+
+      socketIO.emit('join', {message: data.message});
+
+      socketIO.on('message', function(data) {
+        if (Notification.permission === "granted") {
+          var notification = new Notification(data.msg);
+        }
+        else if (Notification.permission !== 'denied') {
+          Notification.requestPermission(function (permission) {
+
+            if (permission === "granted") {
+              var notification = new Notification(data.msg);
+            }
+          });
+        }
+      }, this);
     }
   }
 });
--- server/index.js
+++ server/index.js
@@ -6,16 +6,24 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const http = require('http');
 
 const config = require('./config/config');
-const app = express();
+// const app = express();
+// const server = require('http').createServer(app);
+// const io = require("socket.io").listen(server)
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
 
 function connect(options) {
   return mongoose.connect(config.db, options).connection;
 }
 
 function listen() {
-  app.listen(config.port);
+  server.listen(config.port);
+  // app.listen(config.port);
 
   // eslint-disable-next-line no-console
   console.log(`Express app started on http://127.0.0.1:${config.port}`);
@@ -31,7 +39,7 @@ connect()
  * Expose application
  */
 
-module.exports = app;
+ module.exports = app;
 
 // Configure application
 app.use(cookieParser());
@@ -41,6 +49,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 require('./config/cors')(app);
 require('./config/logger')(app);
 require('./config/passport')(app, passport);
+
+app.all('*', function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", 'http://localhost:4200');
+ res.header("Access-Control-Allow-Headers", "X-Requested-With");
+ res.header('Access-Control-Allow-Headers', 'Content-Type');
+ res.header('Access-Control-Allow-Credentials', 'true');
+ next();
+});
+
+//notifications
+io.sockets.on("connection", function (socket) {
+  console.log('You have connected to the server');
+  socket.on('join', function (data) {
+    console.log(data);
+    console.log(data.message);
+    socket.broadcast.emit('message', {msg: data.message});
+  });
+});
 
 // Bootstrap routes
 require('./config/routes')(app);
