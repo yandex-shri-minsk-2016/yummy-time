09735ad5bdca2869341c9bfc69ec9b95e5857878
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-04-30 15:51:29 +0300

    Show total money in order preview

--- client/app/controllers/new-portion-order.js
+++ client/app/controllers/new-portion-order.js
@@ -7,10 +7,8 @@ export default Ember.Controller.extend({
       portion.set('order', order);
       portion.set('owner', account);
       portion.save().then(() => {
-        const required = order.get('money.required');
         const total = order.get('money.total') + portion.get('cost');
         order.set('money.total', total);
-        order.set('money.required', (total > required) ? total : required);
 
         order.get('portions').pushObject(portion);
         order.save();
