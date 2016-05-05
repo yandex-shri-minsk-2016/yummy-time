8e531ff79c552b10f5a6d87e6c47278a5e5133db
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-05 15:49:50 +0300

    Allow toggle portion paid attr, as per #81

--- client/app/components/b-portion.js
+++ client/app/components/b-portion.js
@@ -2,5 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
-  classNames: ['b-portion']
+  classNames: ['b-portion'],
+
+  click(e) {
+    const target = Ember.$(e.target);
+    if (target.hasClass('checkbox__text') || target.hasClass('checkbox__control')) {
+      this.togglePaidStatus(this.get('portion'));
+    }
+    return false;
+  },
+
+  togglePaidStatus(portion) {
+    portion.toggleProperty('paid');
+    portion.save();
+  }
 });
