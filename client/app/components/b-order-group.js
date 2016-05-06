8cb5c696f701af66642cd965d122b4edac311397
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-06 10:58:07 +0300

    Check group checkbox when all portions are paid

--- client/app/components/b-order-group.js
+++ client/app/components/b-order-group.js
@@ -4,6 +4,10 @@ export default Ember.Component.extend({
   tagName: 'li',
   classNames: ['b-order__item'],
 
+  allPaid: Ember.computed('portions.@each.paid', function() {
+    return this.get('portions').isEvery('paid', true);
+  }),
+
   actions: {
     sayHi() {
       console.log('hi, b-order-group');
