63ff124d55a9c74023817e5f09c456c90e46bf04
octocrux <cate.petrusevich@gmail.com>
2016-05-06 20:47:28 +0300

    Add validated-selectize component

---
+++ client/app/components/validated-selectize.js
@@ -0,0 +1,45 @@
+import Ember from 'ember';
+
+const {
+  computed,
+  defineProperty
+} = Ember;
+
+export default Ember.Component.extend({
+  classNames: ['f-default__row'],
+
+  model: null,
+  type: 'text',
+  placeholder: '',
+  value: null,
+  valuePath: '',
+  validation: null,
+
+  init() {
+    this._super(...arguments);
+    var valuePath = this.get('valuePath');
+    defineProperty(this, 'validation', computed.oneWay(`model.validations.attrs.${valuePath}`));
+    defineProperty(this, 'value', computed.alias(`model.${valuePath}`));
+  },
+
+  actions: {
+    selectItem(content) {
+      this.attrs.selectItem(content);
+    },
+
+    validateField() {
+      this.get('model').validate({on: this.get('valuePath')});
+    }
+  },
+
+  notValidating: computed.not('validation.isValidating'),
+  didValidate: computed.oneWay('targetObject.didValidate'),
+  hasContent: computed.notEmpty('value'),
+  isValid: computed.oneWay('validation.isValid'),
+  isInvalid: computed.oneWay('validation.isInvalid'),
+
+  showMessage: computed('validation.isDirty', 'didValidate', 'isInvalid', function() {
+    return (this.get('validation.isDirty') || this.get('didValidate') && this.get('isInvalid'));
+  })
+
+});
