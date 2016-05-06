6f788986580d4e66cf5392d7b57cc9f846c1913e
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-06 11:09:43 +0300

    Move grouped portions into b-order-group component
    Update stylesheet as well.

--- client/app/components/b-order-group.js
+++ client/app/components/b-order-group.js
@@ -1,12 +1,30 @@
+/* global $ */
 import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
-  classNames: ['b-order__item'],
+  classNames: ['b-order-group'],
 
   allPaid: Ember.computed('portions.@each.paid', function() {
     return this.get('portions').isEvery('paid', true);
   }),
+
+  click: (e) => {
+    const target = $(e.target);
+
+    if (target.hasClass('checkbox__text') || target.hasClass('checkbox__control')) {
+      e.stopPropagation();
+      return;
+    }
+
+    const elem = (e.target === this) ? target : target.parents('.b-order-group');
+
+    if (!elem.length) {
+      return;
+    }
+
+    elem.find('.b-order-group__summary').slideToggle(100);
+  },
 
   actions: {
     sayHi() {
--- client/app/components/b-order.js
+++ client/app/components/b-order.js
@@ -1,25 +1,6 @@
-/* global $ */
-
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  click: (e) => {
-    const target = $(e.target);
-
-    if (target.hasClass('checkbox__text') || target.hasClass('checkbox__control')) {
-      e.stopPropagation();
-      return;
-    }
-
-    const elem = (e.target === this) ? target : target.parents('.b-order__item');
-
-    if (!elem.length) {
-      return;
-    }
-
-    elem.find('.b-order__summary').slideToggle(100);
-  },
-
   actions: {
     toggleActiveState() {
       const order = this.get('order');
