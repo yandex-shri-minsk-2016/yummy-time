b05e5dd23e5a32b9636e1ad8d7a0836979507d2f
octocrux <cate.petrusevich@gmail.com>
2016-05-06 19:49:30 +0300

    Apply new validated-input to f-order form fields

--- client/app/components/validated-input.js
+++ client/app/components/validated-input.js
@@ -6,5 +6,37 @@ const {
 } = Ember;
 
 export default Ember.Component.extend({
-  classNames: ['f-default__row']
+  classNames: ['f-default__row'],
+
+  model: null,
+  type: 'text',
+  placeholder: '',
+  value: null,
+  valuePath: '',
+  validation: null,
+  isTyping: false,
+
+  init() {
+    var valuePath = this.get('valuePath'); // eslint-disable-line
+    this._super(...arguments); // eslint-disable-line prefer-rest-params
+    defineProperty(this, 'validation', computed.oneWay(`model.validations.attrs.${valuePath}`));
+    defineProperty(this, 'value', computed.alias(`model.${valuePath}`));
+  },
+
+  actions: {
+    validateField() {
+      this.get('model').validate({ on: this.get('valuePath') });
+    }
+  },
+
+  notValidating: computed.not('validation.isValidating'),
+  didValidate: computed.oneWay('targetObject.didValidate'),
+  hasContent: computed.notEmpty('value'),
+  isValid: computed.oneWay('validation.isValid'),
+  isInvalid: computed.oneWay('validation.isInvalid'),
+
+  showMessage: computed('validation.isDirty', 'didValidate', 'isInvalid', function() {
+    return (this.get('validation.isDirty') || this.get('didValidate') && this.get('isInvalid'));
+  })
+
 });
