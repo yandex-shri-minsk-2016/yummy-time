f8529668732ca803da98cc7eef3029f67592520f
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-09 20:27:33 +0300

    Use the rest parameters instead of arguments in account serializer

--- client/app/serializers/account.js
+++ client/app/serializers/account.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
-  serialize() {
-    const json = this._super.apply(this, arguments);
+  serialize(...args) {
+    const json = this._super.apply(this, args);
     const password = json.data.attributes.password;
     if (typeof password === 'undefined' || password === null) {
       delete json.data.attributes.password;
