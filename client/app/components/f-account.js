f30de684c1c284a0465e4d61e8c4787f2432c2ee
octocrux <cate.petrusevich@gmail.com>
2016-05-12 23:42:52 +0300

    Add client validations to account settings form

--- client/app/components/f-account.js
+++ client/app/components/f-account.js
@@ -1,9 +1,34 @@
 import Ember from 'ember';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-export default Ember.Component.extend({
+const Validations = buildValidations({
+  'account.name': {
+    validators: [validator('presence', true)]
+  },
+  'account.email': {
+    validators: [
+      validator('presence', true),
+      validator('format', {
+        type: 'email'
+      })
+    ]
+  },
+  'account.phone': {
+    validators: [validator('presence', true)]
+  }
+});
+
+export default Ember.Component.extend(Validations, {
+  didValidate: false,
+
   actions: {
     submit() {
-      return this.attrs.submit();
+      this.validate().then(({ validations }) => {
+        if (validations.get('isValid')) {
+          this.attrs.submit();
+        }
+        this.set('didValidate', true);
+      });
     }
   }
 });
