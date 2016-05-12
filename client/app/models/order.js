60fab74d1ab2bfa9f3712f611e8c8b337eac1c6f
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-12 16:58:45 +0300

    Extract logic to add portion to order into model

--- client/app/models/order.js
+++ client/app/models/order.js
@@ -12,5 +12,13 @@ export default DS.Model.extend({
 
   isReady: Ember.computed('money.total', 'money.required', function() {
     return this.get('money.total') >= this.get('money.required');
-  })
+  }),
+
+  addPortion(portion) {
+    const total = this.get('money.total');
+    const cost = portion.get('cost');
+
+    this.set('money.total', total + cost);
+    this.get('portions').pushObject(portion);
+  }
 });
