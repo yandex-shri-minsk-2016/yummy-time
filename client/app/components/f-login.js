724faa0704fc7f3ac32b93fd0d1dad275a336808
octocrux <cate.petrusevich@gmail.com>
2016-05-12 23:25:06 +0300

    Add client validation to login form

--- client/app/components/f-login.js
+++ client/app/components/f-login.js
@@ -1,13 +1,34 @@
 import Ember from 'ember';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-export default Ember.Component.extend({
+const Validations = buildValidations({
+  identification: {
+    validators: [validator('presence', true)],
+    message: 'Введите логин'
+  },
+  password: {
+    validators: [validator('presence', true)],
+    message: 'Введите пароль'
+  }
+});
+
+export default Ember.Component.extend(Validations, {
+  identification: null,
+  password: null,
+  didValidate: false,
+
   actions: {
     authenticateVia(provider) {
       this.attrs.authenticateVia(provider);
     },
 
     submit() {
-      this.attrs.submit(this.getProperties('identification', 'password'));
+      this.validate().then(({ validations }) => {
+        if (validations.get('isValid')) {
+          this.attrs.submit(this.getProperties('identification', 'password'));
+        }
+      });
+      this.set('didValidate', true);
     }
   }
 });
