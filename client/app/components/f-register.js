c9800b21b9d742e336568ac72fae2c2c757bf3b0
octocrux <cate.petrusevich@gmail.com>
2016-05-12 23:25:49 +0300

    Add client validation to register form

--- client/app/components/f-register.js
+++ client/app/components/f-register.js
@@ -1,13 +1,41 @@
 import Ember from 'ember';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-export default Ember.Component.extend({
+const Validations = buildValidations({
+  name: {
+    validators: [validator('presence', true)]
+  },
+  email: {
+    validators: [
+      validator('presence', true),
+      validator('format', {
+        type: 'email'
+      })
+    ]
+  },
+  password: {
+    validators: [validator('presence', true)]
+  },
+  phone: {
+    validators: [validator('presence', true)]
+  }
+});
+
+export default Ember.Component.extend(Validations, {
+  didValidate: false,
+
   actions: {
     submit() {
-      this.attrs.submit({
-        email: this.get('email'),
-        name: this.get('name'),
-        password: this.get('password'),
-        phone: this.get('phone')
+      this.validate().then(({ validations }) => {
+        if (validations.get('isValid')) {
+          this.attrs.submit({
+            email: this.get('email'),
+            name: this.get('name'),
+            password: this.get('password'),
+            phone: this.get('phone')
+          });
+        }
+        this.set('didValidate', true);
       });
     }
   }
