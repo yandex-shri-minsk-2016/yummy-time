5baf0310ae82b2d4d0c18df65a928683c46f0e39
octocrux <cate.petrusevich@gmail.com>
2016-05-13 18:06:15 +0300

    Add deploy scripts for client and server

---
+++ deploy-client.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+PROJ=$MYPROJ
+
+cd $PROJ
+mkdir tmp
+
+cd tmp
+INNER=$(pwd)
+
+if [ $INNER == $PROJ ]; then
+  echo $PROJ
+  echo $INNER
+  echo 'Failed to change the directory'
+  exit 1
+fi
+
+rm -rf client/
+echo | pwd
+
+mkdir client
+
+cd $PROJ
+
+cp -rf ./client/app tmp/client/app
+cp -rf ./client/config tmp/client/config
+cp -rf ./client/public tmp/client/public
+cp -f ./client/ember-cli-build.js tmp/client/
+cp -f ./client/bower.json tmp/client/
+cp -f ./client/package.json tmp/client/
+
+cd $PROJ/tmp/client/
+
+if [ $INNER == $PROJ ]; then
+  echo $PROJ
+  echo $INNER
+  echo 'Failed to change the directory'
+  exit 1
+fi
+
+git init
+git add --all
+git cm 'Deploy Client'
+git remote add client 'https://git.heroku.com/yummy-time.git'
+git push client master --force
+
+exit
---
+++ deploy-server.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+PROJ=$MYPROJ
+
+cd $PROJ
+mkdir tmp
+
+cd tmp
+INNER=$(pwd)
+
+if [ $INNER == $PROJ ]; then
+  echo $PROJ
+  echo $INNER
+  echo 'Failed to change the directory'
+  exit 1
+fi
+
+rm -rf server/
+mkdir server
+
+cd $PROJ
+cp -rf ./server/app tmp/server/app
+cp -rf ./server/config tmp/server/config
+cp -rf ./server/index.js tmp/server/index.js
+cp -rf ./server/package.json tmp/server/package.json
+
+cd $PROJ/tmp/server/
+
+if [ $INNER == $PROJ ]; then
+  echo $PROJ
+  echo $INNER
+  echo 'Failed to change the directory'
+  exit 1
+fi
+
+git init
+git add --all
+git cm 'Deploy Server'
+git remote add server 'https://git.heroku.com/yummy-server.git'
+git push server master --force
+
+exit
