892647e208cf26b8eb9e2e147a5634dbc315241f
octocrux <cate.petrusevich@gmail.com>
2016-05-13 21:10:08 +0300

    Move API namespace to config

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
+import ENV from '../config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:token',
-  namespace: 'api/v1'
+  namespace: ENV.namespace
 });
