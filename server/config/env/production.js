849ee54a4c4625e24f0310d6f74a65495dd41e88
Vital Kudzelka <vital.kudzelka@gmail.com>
2016-05-14 00:15:39 +0300

    Add custom production config for Google api

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -3,5 +3,11 @@
 module.exports = {
   db: `mongodb://${process.env.YUMMY_USER}:${process.env.YUMMY_PASSWORD}@ds011382.mlab.com:11382/yummydb`,
   port: process.env.PORT || 3000,
-  secret: 'secret'
+  secret: 'secret',
+  google: {
+    // eslint-disable-next-line max-len
+    clientID: process.env.GOOGLE_CLIENTID || '1071029381615-tucu38j7kboh2kk4f8tj9br832gihl03.apps.googleusercontent.com',
+    clientSecret: process.env.GOOGLE_SECRET || 'mAgQY2Z9TPJzFGEGkQjU1-37',
+    callbackURL: 'https://yummy-time.herokuapp.com'
+  }
 };
