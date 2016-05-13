6184386ce14dc200edbda92a6264dc4f64e9ba2f
octocrux <cate.petrusevich@gmail.com>
2016-05-14 00:01:22 +0300

    Inherit b-input from b-base-input

--- client/app/components/b-input.js
+++ client/app/components/b-input.js
@@ -1,41 +1,4 @@
-import Ember from 'ember';
+import BaseInput from './b-base-input';
 
-const {
-  computed,
-  defineProperty
-} = Ember;
-
-export default Ember.Component.extend({
-  classNames: ['f-default__row'],
-
-  model: null,
-  type: 'text',
-  placeholder: '',
-  value: null,
-  valuePath: '',
-  validation: null,
-  isTyping: false,
-
-  notValidating: computed.not('validation.isValidating'),
-  didValidate: computed.oneWay('targetObject.didValidate'),
-  hasContent: computed.notEmpty('value'),
-  isValid: computed.oneWay('validation.isValid'),
-  isInvalid: computed.oneWay('validation.isInvalid'),
-
-  showMessage: computed('didValidate', 'isInvalid', function() {
-    return (this.get('didValidate') && this.get('isInvalid'));
-  }),
-
-  init() {
-    const valuePath = this.get('valuePath');
-    this._super(...arguments); // eslint-disable-line prefer-rest-params
-    defineProperty(this, 'validation', computed.oneWay(`model.validations.attrs.${valuePath}`));
-    defineProperty(this, 'value', computed.alias(`model.${valuePath}`));
-  },
-
-  actions: {
-    validateField() {
-      this.get('model').validate({ on: this.get('valuePath') });
-    }
-  }
+export default BaseInput.extend({
 });
