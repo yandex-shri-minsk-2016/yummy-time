fb637ee85bb6d19bc255226679f6fe56b7cd602c
rei-li <rei-li@mail.ru>
2016-05-14 12:28:09 +0300

    fix rebase issues

--- server/config/cors.js
+++ server/config/cors.js
@@ -2,12 +2,11 @@
 const config = require('./config');
 
 module.exports = function cors(app) {
-  app.use((req, res, next) => {
+  app.all('*', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', config.originURL);
     res.header('Access-Control-Allow-Headers',
                'Content-Type,Content-Length,X-Requested-With,Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
   });
